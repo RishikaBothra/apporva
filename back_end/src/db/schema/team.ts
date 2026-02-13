33bf6a3a3e8cdab0ef08ecaa15d0d5e16e3c5b44
Rishika Bothra <rshkbothra@gmail.com>
2026-02-13 19:32:20 +0530

    feat: add teams table schema

---
+++ back_end/src/db/schema/team.ts
@@ -0,0 +1,15 @@
+import { PgTable,integer,pgTable,timestamp, varchar,serial } from "drizzle-orm/pg-core";
+import { user } from "./user";
+
+export const team = pgTable("team",{
+
+    id: serial("id").primaryKey(),
+    name: varchar("name",{length:255}).notNull(),
+    created_by:integer("created_by")
+    .references(()=>user.id)
+    .notNull(),
+    created_at:timestamp("created_at")
+    .defaultNow()
+    .notNull()
+
+})
--- back_end/src/db/schema/user.ts
+++ back_end/src/db/schema/user.ts
@@ -1,7 +1,7 @@
 import { pgTable, varchar, timestamp, serial, pgEnum } from "drizzle-orm/pg-core";
 export const roleEnum = pgEnum("role", ["employee", "manager", "admin"]);
 
-export const users = pgTable("users", {
+export const user = pgTable("users", {
     id: serial().primaryKey(),
     fullName: varchar("full_name", { length: 255 }).notNull(),
     email: varchar("email", { length: 255 }).notNull().unique(),
