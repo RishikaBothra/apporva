4472fb2493fc57c66240962b7939196fefec5ca9
Rishika Bothra <rshkbothra@gmail.com>
2026-02-13 19:36:18 +0530

    upadate the fromate of the file team.ts

--- back_end/src/db/schema/team.ts
+++ back_end/src/db/schema/team.ts
@@ -1,15 +1,17 @@
-import { PgTable,integer,pgTable,timestamp, varchar,serial } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  timestamp,
+  varchar,
+  serial,
+} from "drizzle-orm/pg-core";
 import { user } from "./user";
 
-export const team = pgTable("team",{
-
-    id: serial("id").primaryKey(),
-    name: varchar("name",{length:255}).notNull(),
-    created_by:integer("created_by")
-    .references(()=>user.id)
+export const team = pgTable("team", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  created_by: integer("created_by")
+    .references(() => user.id)
     .notNull(),
-    created_at:timestamp("created_at")
-    .defaultNow()
-    .notNull()
-
-})
+  created_at: timestamp("created_at").defaultNow().notNull(),
+});
